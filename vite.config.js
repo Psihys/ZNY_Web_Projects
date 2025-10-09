470d71bf518732509ef3312f54efe2db77579d74
Bohdan Shevchenko <bogdan.s.0474@gmail.com>
2025-10-09 15:54:06 +0200

    Add partials and rebuild website

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'node:path'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import handlebars from 'vite-plugin-handlebars'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src', 'partials'),
+      context: {
+        siteName: 'Labo-6',
+        labName: ' Labo_Number6',
+        studentGroup: '6-1213-1',
+        studentEmail: 'student@example.com',
+      },
+      pages: [
+        {
+          input: resolve(__dirname, 'index.html'),
+          context: { pageTitle: 'Home' },
+        },
+        {
+          input: resolve(__dirname, 'pages/about.html'),
+          context: { pageTitle: 'About' },
+        },
+        {
+          input: resolve(__dirname, 'pages/contact.html'),
+          context: { pageTitle: 'Contact' },
+        },
+      ],
+      reloadOnPartialChange: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
