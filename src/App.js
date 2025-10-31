c0dd702b6af95d414a38437362e6522820e1c5d0
Bohdan Shevchenko <bogdan.s.0474@gmail.com>
2025-10-31 11:44:46 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
 import './styles/App.scss'
 import './styles/tw.css'
 import './scripts/common/index.js'
-import './scripts/api/api.js'
+import './scripts/app/aboutPage.js'
+
+let cleanupFunction = () => {};
+const page = document.body.dataset.page;
+
+const routes = {
+    About:    () => import('./scripts/app/aboutPage.js').then(m => m.initAbout()
+        .then(abortFn => {
+            cleanupFunction = abortFn;
+            console.log('Ініціалізація About Page завершена. Функція скасування збережена.');
+        })),
+};
+
+if (routes[page]) {
+    routes[page]().catch(err => {
+        console.error('Помилка ініціалізації сторінки:', err);
+    });
+}
